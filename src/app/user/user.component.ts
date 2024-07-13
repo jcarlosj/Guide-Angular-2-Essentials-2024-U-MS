b1d69690229b87bf6895d9b691f073dd2bd4f0c0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-07-13 12:38:39 -0500

    [+] dummy-users.ts: Fake Users Data [+] user.component.ts: Despliega el nombre y avatar de un usuario aleatoriamente

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+
+const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +13,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[ randomIndex ];
 }
