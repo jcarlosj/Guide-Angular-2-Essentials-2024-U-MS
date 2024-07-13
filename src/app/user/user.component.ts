500c892b9e9ebd8171d6edf12d31ef83724eb189
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-07-13 18:04:52 -0500

    [+] user.component.ts: Establece que los valores de entrada sean requeridos (Obligatorios)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-
-const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,8 +8,8 @@ const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
 
