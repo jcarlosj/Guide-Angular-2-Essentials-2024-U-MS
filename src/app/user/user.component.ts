bf33226b2eb1d45814bbe82e2706eca119e511a4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-07-13 18:24:42 -0500

    [-] user.component.ts: Establece que los valores de entrada sean requeridos (Obligatorios) sin usar 'signials'

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +8,16 @@ import { Component, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  imagePath = computed( () => 'assets/images/users/' + this.avatar() );
+  get imagePath() {
+
+    return 'assets/images/users/' + this.avatar;
+  }
 
   onSelectUser() {
-    console.log( 'Clicked ' + this.name() + '!' );
+    console.log( 'Clicked ' + this.name + '!' );
   }
 
 }
