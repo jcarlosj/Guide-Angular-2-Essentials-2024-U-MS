6c047d10bf3af41626b4a556ec51023c5f4c9543
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-07-17 09:30:41 -0500

    [+] tasks.component.*: Renderizando condicionalmente el componente 'NewTaskComponent' usando el evento (click) sobre el boton de 'Agregar tarea'

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask: boolean = false;
+
   tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
@@ -22,4 +26,9 @@ export class TasksComponent {
   onCompleteTask( id: string ) {
     this.tasks = this.tasks.filter( task => task.id !== id );
   }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
 }
