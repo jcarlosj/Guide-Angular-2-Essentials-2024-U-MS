3343c6627dcb4c20d290ebbf32128dd3cb3380ba
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-12-30 19:48:10 -0500

    [+] tasks.service: Implementando localStorage para la persistencia de datos

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -9,7 +9,13 @@ import { type NewTaskData } from './task/task.model';
 export class TasksService {
   private tasks = DUMMY_TASKS;
 
-  constructor() { }
+  constructor() {
+    const tasks = localStorage.getItem( 'tasks' );
+
+    if( tasks ) {
+      this.tasks = JSON.parse( tasks );
+    }
+  }
 
   getUserTasks( userId: string ) {
     return this.tasks.filter( task => task.userId === userId );
@@ -23,9 +29,15 @@ export class TasksService {
       summary: newTask.summary,
       dueDate: newTask.date
     });
+    this.saveTasks();
   }
 
   removeTask( id: string ) {
     this.tasks = this.tasks.filter( task => task.id !== id );
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem( 'tasks', JSON.stringify( this.tasks ) );
   }
 }
