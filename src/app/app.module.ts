b7d511cda7bf21d64915b94e7a2db8f4ccc34422
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-12-31 17:00:28 -0500

    Crea modulo principal 'app.module.ts' que usa Standalone Components -> [$] main.ts: platform-browser-dynamic -> bootstrapModule -> AppModule [+] app.module.ts: Define objeto de configuracion para el decorador @NgModule [-] app.component.ts: propiedades (standalone e imports)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 
-
-@NgModule({
-  declarations: [],
+ @NgModule({
+  declarations: [
+    /** Aqui se integran Modular Components */
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ],
   imports: [
-    CommonModule
+    BrowserModule,
+    /** Aqui se integran Standalone Components */
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent
   ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { appConfig } from './app/app.config';
 
-import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+/** Modular project */
+platformBrowserDynamic().bootstrapModule( AppModule );
+
+/** Standalone project */
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
 
